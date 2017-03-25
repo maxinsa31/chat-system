51cf9a9ce5f889575d55a055396a9832f6ef86d8
maxinsa31 <maxime.fargeas@outlook.fr>
2017-03-25 17:30:21 +0100

    Test modification main

--- src/launchChatSystem.java
+++ src/launchChatSystem.java
@@ -7,6 +7,7 @@ public class launchChatSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Bienvenue dans notre chat system");
 		new ConnectionWindow();
 
 	}

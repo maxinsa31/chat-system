d541171f6b29806586ae1521c39c913ba31e2ad0
maxinsa31 <maxime.fargeas@outlook.fr>
2017-04-12 11:06:01 +0200

    Ajout receiver

---
+++ src/model/Receiver.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.net.InetAddress;
+
+public class Receiver {
+
+    private InetAddress IP;
+    
+    private  String pseudo;
+    
+    public Receiver(InetAddress IP, String pseudo){
+   	 this.IP = IP;
+   	 this.pseudo = pseudo;
+    }
+
+    public InetAddress getIP() {
+   	 return IP;
+    }
+
+    public String getPseudo() {
+   	 return pseudo;
+    }
+
+    
+}
